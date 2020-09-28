0b84f4b9022373aeb8cc1252715d1ade19086c59
asma <asmasweed@hotmail.com>
2020-09-28 13:36:22 +0200

    js

---
+++ asma-main.js
@@ -0,0 +1,32 @@
+var buttons = document.querySelectorAll(".status");
+console.log(buttons);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      this.value;
+      console.log(button.dataset.id);
+      studentUpdate(button);
+    });
+  });
+  
+  function studentUpdate(button){
+    var complete = this.value;
+    var gf_id = button.dataset.id;
+    //console.log(button.dataset.id);
+    console.log(complete);
+    
+    jQuery.ajax({
+        url : test.ajax_url,
+        type : 'post',
+        data : {
+            action : 'update_student_status',  //php function that runs        
+            complete : complete,  //variable passed to php and its name
+            gf_id :   gf_id,   //variable passed to php and its name
+        },
+        success : function( response ) {
+           // alert('update success') //tells you it worked
+           
+           
+        }
+    });
+}
