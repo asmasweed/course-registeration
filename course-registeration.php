63643fd7dd914197a00c2cf8238bfa623076b8a6
asma <asmasweed@hotmail.com>
2020-10-26 11:18:01 +0100

    fulll

--- course-registeration.php
+++ course-registeration.php
@@ -79,7 +79,7 @@ function asma_add_content($content){
 function asma_get_full_description($post){
   $post_id = $post->ID;
   if(get_field('full_description',$post_id)){
-    $full = '<div class="full-desc">' .get_field('full_description',$post_id) . '</div>';
+    $full = '<div class="full-desc"><h4>Describtion</h4>' .get_field('full_description',$post_id) . '</div>';
     return $full;
   }
 }
